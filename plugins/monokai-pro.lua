98730a18777829508ff59670b0bd46719a86cd57
Austen Ito <24883+austenito@users.noreply.github.com>
2024-01-18 23:45:21 -0500

    Update color scheme to catppuccin

--- plugins/monokai-pro.lua
+++ plugins/monokai-pro.lua
@@ -3,7 +3,7 @@ return {
     "loctvl842/monokai-pro.nvim",
     config = function()
         require("monokai-pro").setup {
-        filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+        filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
     }
     end
   },
