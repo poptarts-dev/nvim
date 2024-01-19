718f4a5fe75fd7e4df92111aeb029aaf29dddaf9
Austen Ito <24883+austenito@users.noreply.github.com>
2024-01-18 23:45:05 -0500

    add go support

--- init.lua
+++ init.lua
@@ -81,5 +81,15 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+
+    -- gofmt + goimport on save
+    local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+       require('go.format')
+      end,
+      group = format_sync_grp,
+    })
   end,
 }
