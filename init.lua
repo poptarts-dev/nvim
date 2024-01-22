b60fb80d78b36b003e1e935ca17ed70b508d5aed
Austen Ito <24883+austenito@users.noreply.github.com>
2024-01-21 21:50:42 -0500

    Rename colorscheme files and switch to kanagawa

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin",
+  colorscheme = "kanagawa",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
