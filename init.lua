a1a9b603ffd8b6f7f7d219527e66363b456ec220
Austen Ito <24883+austenito@users.noreply.github.com>
2024-01-25 20:57:33 -0500

    Go configuration updates
    * Max line length
    * auto import on save

--- init.lua
+++ init.lua
@@ -81,15 +81,5 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
-
-    -- gofmt + goimport on save
-    local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*.go",
-      callback = function()
-       require('go.format')
-      end,
-      group = format_sync_grp,
-    })
   end,
 }
--- plugins/go.lua
+++ plugins/go.lua
@@ -8,9 +8,19 @@ return {
     },
     config = function()
       require("go").setup({
-        lsp_cfg = false
+        lsp_cfg = false,
+        max_line_len = 160
       })
 
+      -- gofmt + goimport on save
+      local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+         require('go.format').goimport()
+        end,
+        group = format_sync_grp,
+      })
       local cfg = require'go.lsp'.config() -- config() return the go.nvim gopls setup
       require('lspconfig').gopls.setup(cfg)
     end,
