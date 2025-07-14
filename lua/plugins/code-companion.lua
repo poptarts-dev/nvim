31c1e418e9030c4170b6e168167ff57eb350c9b3
aito <a@poptarts.dev>
2025-07-13 22:46:20 -0400

    Add code companion

---
+++ lua/plugins/code-companion.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    lazy = false,
+    config = function()
+      require("codecompanion").setup {
+        adapters = {
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              env = {
+                api_key = "cmd: echo $ANTHROPIC_KEY",
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = "anthropic",
+          },
+          inline = {
+            adapter = "anthropic",
+          },
+        },
+        display = {
+          diff = {
+            provider = "mini_diff",
+          },
+          chat = {
+            window = {
+              position = "right",
+            },
+          },
+        },
+      }
+
+      vim.keymap.set("n", "<Leader>lo", ":CodeCompanionChat<CR>", { desc = "Code Companion Chat Toggle" })
+      vim.keymap.set("n", "<Leader>lp", ":CodeCompanionActions<CR>", { desc = "Code Companion Chat Actions" })
+    end,
+  },
+}
